8214dacfb59bf79bcdb731aceb761fb45d87e139
schuemie <schuemie@ohdsi.org>
2024-08-27 12:47:05 +0200

    Adding all DBs. Removing grid if no data

--- PlotsAndTableFunctions.R
+++ PlotsAndTableFunctions.R
@@ -24,7 +24,7 @@ createPerDbResultsTable <- function(target, comparator, outcome, connection) {
                                               schema = schema,
                                               snakeCaseToCamelCase = TRUE)[, 1]
 
-    # Equipoise
+    # Equipoise ------------------------------------------------------------------------------------
     sql <- "
     SELECT database_id,
       preference_score,
@@ -52,6 +52,8 @@ createPerDbResultsTable <- function(target, comparator, outcome, connection) {
                                     density = comparatorDensity) |>
                              mutate(type = "Comparator")
     )
+    vizData <- vizData |>
+        full_join(tibble(databaseId = databaseIds), by = join_by(databaseId))
     vizData$databaseId <- databaseIdToFactor(vizData$databaseId, databaseIds)
     vizData$type <- factor(vizData$type, levels = sort(unique(vizData$type), decreasing = FALSE))
 
@@ -59,19 +61,24 @@ createPerDbResultsTable <- function(target, comparator, outcome, connection) {
         filter(targetId == target,
                comparatorId == comparator,
                outcomeId == outcome,
-               analysisId == 5) |>
-        mutate(label = if_else(is.na(minEquipoise),
-                               NA,
-                               sprintf("Equipoise = %0.2f", minEquipoise)))
+               analysisId == 5,
+               !is.na(minEquipoise)) |>
+        mutate(label = sprintf("Equipoise = %0.2f", minEquipoise),
+               origin = 0)
     vizDbData$databaseId <- databaseIdToFactor(vizDbData$databaseId, databaseIds)
-    labelY <- max(vizData$density) + 0.5
+    labelY <- max(vizData$density, na.rm = TRUE) + 0.5
+    breaks <- c(0, 0.5, 1)
+    breaksDb <- vizDbData |>
+        select(databaseId) |>
+        cross_join(tibble(x = breaks))
 
-    plot1 <- ggplot(vizData, aes(x = preferenceScore, y = density)) +
+    plotEquipoise <- ggplot(vizData, aes(x = preferenceScore, y = density)) +
+        geom_segment(aes(x = x, y = 0, xend = x, yend = labelY), data = breaksDb, color = "lightgray") +
         geom_area(aes(color = type, fill = type), position = "identity", alpha = 0.5) +
-        geom_hline(yintercept = 0) +
-        geom_label(aes(label = label), x = 0.5, y = labelY, vjust = 1, label.size = 0, data = vizDbData) +
+        geom_hline(aes(yintercept = origin), data = vizDbData) +
+        geom_label(aes(label = label), x = 0.5, y = labelY, vjust = 1, label.size = 0, size = 3, data = vizDbData) +
         coord_cartesian(xlim = c(0, 1), ylim = c(0, labelY)) +
-        scale_x_continuous("Preference score") +
+        scale_x_continuous("Preference score", breaks = breaks) +
         scale_fill_manual(values = c(alpha("#336B91", 0.6), alpha("#EB6622", 0.6))) +
         scale_color_manual(values = c(alpha("#336B91", 0.7), alpha("#EB6622", 0.7))) +
         facet_grid(databaseId~., switch = "y") +
@@ -79,9 +86,7 @@ createPerDbResultsTable <- function(target, comparator, outcome, connection) {
             axis.text.y = element_blank(),
             axis.title.y = element_blank(),
             axis.ticks.y = element_blank(),
-            panel.grid.major.y = element_blank(),
-            panel.grid.minor.y = element_blank(),
-            panel.grid.major.x = element_line(color = "lightgray"),
+            panel.grid = element_blank(),
             panel.background = element_blank(),
             strip.background = element_blank(),
             strip.text.y.left = element_text(angle = 0, hjust = 1),
@@ -89,10 +94,10 @@ createPerDbResultsTable <- function(target, comparator, outcome, connection) {
             legend.title = element_blank(),
             legend.key.size = unit(0.25, "cm")
         )
-    plot1
+    plotEquipoise
 
 
-    # Covariate balance
+    # Covariate balance ----------------------------------------------------------------------------
     sql <- "
     SELECT database_id,
       PERCENTILE_DISC(ARRAY[0, 0.25,0.5,0.75,1]) WITHIN GROUP (ORDER BY std_diff_before) AS percentiles_before,
@@ -118,6 +123,8 @@ createPerDbResultsTable <- function(target, comparator, outcome, connection) {
                            stringToVars(balance$percentilesAfter),
                            type = "After matching",
                            y = .5))
+    vizData <- vizData |>
+        full_join(tibble(databaseId = databaseIds), by = join_by(databaseId))
     vizData$databaseId <- databaseIdToFactor(vizData$databaseId, databaseIds)
     vizData$type <- factor(vizData$type, levels = sort(unique(vizData$type), decreasing = TRUE))
 
@@ -125,34 +132,41 @@ createPerDbResultsTable <- function(target, comparator, outcome, connection) {
         filter(targetId == target,
                comparatorId == comparator,
                outcomeId == outcome,
-               analysisId == 5) |>
-        mutate(label = if_else(is.na(maxAbsStdDiffMean),
-                                     NA,
-                                     sprintf("Max ASDM = %0.2f", maxAbsStdDiffMean)))
+               analysisId == 5,
+               !is.na(maxAbsStdDiffMean)) |>
+        mutate(label = sprintf("Max ASDM = %0.2f", maxAbsStdDiffMean),
+               origin = 0,
+               minX = -0.15,
+               maxX = 0.15,
+               median = 0,
+               y = 0)
     vizDbData$databaseId <- databaseIdToFactor(vizDbData$databaseId, databaseIds)
 
-    boxPlotHeight <- 0.2
-    balanceThreshold <- 0.1
+    breaks <- c(-0.5, 0, 0.5)
+    breaksDb <- vizDbData |>
+        select(databaseId) |>
+        cross_join(tibble(x = breaks))
 
-    plot2 <- ggplot(vizData, aes(x = median, y = y)) +
-        geom_rect(xmin = -balanceThreshold, ymin = 0, xmax = balanceThreshold, ymax = 1.5, alpha = 0.1, size = 0) +
-        geom_vline(xintercept = 0) +
+    boxPlotHeight <- 0.2
+
+    plotBalance <- ggplot(vizData, aes(x = median, y = y)) +
+        geom_segment(aes(x = x, y = 0, xend = x, yend = 2), data = breaksDb, color = "lightgray") +
+        geom_rect(aes(xmin = minX, ymin = 0, xmax = maxX, ymax = 1.5), alpha = 0.1, size = 0, data = vizDbData) +
+        geom_vline(aes(xintercept = origin), data = vizDbData) +
         geom_errorbarh(aes(xmin = min, xmax = max, color = type), height = boxPlotHeight*2) +
         geom_rect(aes(xmin = lower, ymin=y-boxPlotHeight, xmax = upper, ymax = y+boxPlotHeight, color = type, fill = type)) +
         geom_segment(aes(x = median, y = y-boxPlotHeight, xend = median, yend = y+boxPlotHeight, color = type), size = 1) +
-        geom_label(aes(label = label), x = 0, y = 2, vjust = 1, label.size = 0, data = vizDbData) +
+        geom_label(aes(label = label), x = 0, y = 2, vjust = 1, label.size = 0, size = 3, data = vizDbData) +
         coord_cartesian(xlim = c(-1, 1), ylim = c(0.25, 2)) +
-        scale_x_continuous("Std. diff. means") +
-        scale_color_manual(values = c(alpha("#e5ae38", 0.6), alpha("#6d904f", 0.6))) +
-        scale_fill_manual(values = c(alpha("#e5ae38", 0.6), alpha("#6d904f", 0.6))) +
+        scale_x_continuous("Std. diff. means", breaks = breaks) +
+        scale_color_manual(values = c(alpha("#e5ae38", 0.6), alpha("#6d904f", 0.6)), na.translate = FALSE) +
+        scale_fill_manual(values = c(alpha("#e5ae38", 0.6), alpha("#6d904f", 0.6)), na.translate = FALSE) +
         facet_grid(databaseId~., switch = "y") +
         theme(
             axis.text.y = element_blank(),
             axis.title.y = element_blank(),
             axis.ticks.y = element_blank(),
-            panel.grid.major.y = element_blank(),
-            panel.grid.minor.y = element_blank(),
-            panel.grid.major.x = element_line(color = "lightgray"),
+            panel.grid = element_blank(),
             panel.background = element_blank(),
             strip.background = element_blank(),
             strip.text.y.left = element_blank(),
@@ -160,27 +174,35 @@ createPerDbResultsTable <- function(target, comparator, outcome, connection) {
             legend.title = element_blank(),
             legend.key.size = unit(0.25, "cm")
         )
-    plot2
+    plotBalance
 
 
-    # MDRR
+    # MDRR -----------------------------------------------------------------------------------------
     vizData <- diagnostics |>
         filter(targetId == target,
                comparatorId == comparator,
                outcomeId == outcome,
-               analysisId == 2) |>
-        mutate(label = if_else(is.na(mdrr),
-                               NA,
-                               sprintf("MDRR = %0.1f", mdrr)),
-               type = "MDRR")
-    vizData$databaseId <- databaseIdToFactor(vizDbData$databaseId, databaseIds)
-    breaks <- c(0, 4, 10)
+               analysisId == 2,
+               !is.na(mdrr)) |>
+        mutate(label = sprintf("MDRR = %0.1f", mdrr),
+               origin = 1,
+               type = "MDRR",
+               threshold = 4)
+    vizData <- vizData |>
+        full_join(tibble(databaseId = databaseIds), by = join_by(databaseId))
+    vizData$databaseId <- databaseIdToFactor(vizData$databaseId, databaseIds)
+    breaks <- c(1, 4, 10)
+    breaksDb <- vizData |>
+        filter(!is.na(mdrr)) |>
+        select(databaseId) |>
+        cross_join(tibble(x = breaks))
     plotMdrr <- ggplot(vizData) +
-        geom_rect(xmin = 0, ymin = 0, xmax = 4, ymax = 1.5, alpha = 0.1, size = 0) +
-        geom_vline(xintercept = 0) +
-        geom_rect(aes(xmax = mdrr, color = type, fill = type), xmin = 0, ymin = 0.25, ymax = 1.25, alpha = 0.5) +
-        geom_label(aes(label = label), x = 2, y = 2, vjust = 1, label.size = 0) +
-        coord_cartesian(xlim = c(0, 10), ylim = c(0, 2)) +
+        geom_segment(aes(x = x, y = 0, xend = x, yend = 2), data = breaksDb, color = "lightgray") +
+        geom_rect(aes(xmin = 1, ymin = 0, xmax = threshold, ymax = 1), alpha = 0.1, size = 0) +
+        geom_vline(aes(xintercept = origin)) +
+        geom_rect(aes(xmax = mdrr, color = type, fill = type), xmin = 1, ymin = 0.25, ymax = 0.75, alpha = 0.5) +
+        geom_label(aes(label = label), x = 5.5, y = 2, vjust = 1, label.size = 0, size = 3) +
+        coord_cartesian(xlim = c(1, 10), ylim = c(0, 2)) +
         scale_x_continuous("MDRR", breaks = breaks) +
         scale_color_manual(values = "#336B91") +
         scale_fill_manual(values = "#336B91") +
@@ -189,9 +211,7 @@ createPerDbResultsTable <- function(target, comparator, outcome, connection) {
             axis.text.y = element_blank(),
             axis.title.y = element_blank(),
             axis.ticks.y = element_blank(),
-            panel.grid.major.y = element_blank(),
-            panel.grid.minor.y = element_blank(),
-            panel.grid.major.x = element_line(color = "lightgray"),
+            panel.grid = element_blank(),
             panel.background = element_blank(),
             strip.background = element_blank(),
             strip.text.y.left = element_blank(),
@@ -203,7 +223,7 @@ createPerDbResultsTable <- function(target, comparator, outcome, connection) {
     plotMdrr
 
 
-    # EASE
+    # EASE -----------------------------------------------------------------------------------------
     negativeControlIds <- renderTranslateQuerySql(connection = connection,
                                                   sql = "SELECT outcome_id FROM @schema.negative_control_outcome;",
                                                   schema = schema,
@@ -229,23 +249,32 @@ createPerDbResultsTable <- function(target, comparator, outcome, connection) {
     vizData <- ncs |>
         filter(!grepl("Meta-analysis", databaseId)) |>
         mutate(type = "Negative control")
+    vizData <- vizData |>
+        full_join(tibble(databaseId = databaseIds), by = join_by(databaseId))
     vizData$databaseId <- databaseIdToFactor(vizData$databaseId, databaseIds)
 
     vizDbData <- diagnostics |>
         filter(targetId == target,
                comparatorId == comparator,
                outcomeId == outcome,
-               analysisId == 5) |>
-        mutate(label = sprintf("Ease = %0.2f", ease))
+               analysisId == 5,
+               !is.na(ease)) |>
+        mutate(label = sprintf("Ease = %0.2f", ease),
+               origin = 0,
+               slope = 1/1.96)
     vizDbData$databaseId <- databaseIdToFactor(vizDbData$databaseId, databaseIds)
     breaks <- c(0.5, 1, 2)
-    plot3 <- ggplot(vizData, aes(x = logRr, y = seLogRr)) +
-        geom_abline(slope = 1/1.96, linetype = "dashed") +
-        geom_abline(slope = -1/1.96, linetype = "dashed") +
-        geom_hline(yintercept = 0) +
-        geom_vline(xintercept = 0) +
+    breaksDb <- vizDbData |>
+        select(databaseId) |>
+        cross_join(tibble(x = log(breaks)))
+    plotEase <- ggplot(vizData, aes(x = logRr, y = seLogRr)) +
+        geom_segment(aes(x = x, y = 0, xend = x, yend = 2), data = breaksDb, color = "lightgray") +
+        geom_abline(aes(slope = slope, intercept = 0), linetype = "dashed", data = vizDbData) +
+        geom_abline(aes(slope = -slope, intercept = 0), linetype = "dashed", data = vizDbData) +
+        geom_hline(aes(yintercept = origin), data = vizDbData) +
+        geom_vline(aes(xintercept = origin), data = vizDbData) +
         geom_point(aes(color = type), shape = 16, alpha = 0.5) +
-        geom_label(aes(label = label), x = 0, y = 1, vjust = 1, label.size = 0, data = vizDbData) +
+        geom_label(aes(label = label), x = 0, y = 1, vjust = 1, label.size = 0, size = 3, data = vizDbData) +
         coord_cartesian(xlim = log(c(0.25, 4)), ylim = c(0, 1)) +
         scale_x_continuous("Hazard ratio", breaks = log(breaks), labels = breaks) +
         scale_color_manual(values = "#336B91") +
@@ -254,9 +283,7 @@ createPerDbResultsTable <- function(target, comparator, outcome, connection) {
             axis.text.y = element_blank(),
             axis.title.y = element_blank(),
             axis.ticks.y = element_blank(),
-            panel.grid.major.y = element_blank(),
-            panel.grid.minor.y = element_blank(),
-            panel.grid.major.x = element_line(color = "lightgray"),
+            panel.grid = element_blank(),
             panel.background = element_blank(),
             strip.background = element_blank(),
             strip.text.y.left = element_blank(),
@@ -264,9 +291,9 @@ createPerDbResultsTable <- function(target, comparator, outcome, connection) {
             legend.title = element_blank(),
             legend.key.size = unit(0.25, "cm")
         )
-    plot3
+    plotEase
 
-    # Estimate
+    # Estimate -------------------------------------------------------------------------------------
     sql <- "
     SELECT database_id,
       calibrated_rr,
@@ -289,15 +316,23 @@ createPerDbResultsTable <- function(target, comparator, outcome, connection) {
         filter(!grepl("Meta-analysis", databaseId)) |>
         mutate(type = "Calibrated estimate",
                label = if_else(is.na(calibratedRr), NA,
-                               sprintf("%0.2f (%0.2f - %0.2f)", calibratedRr, calibratedCi95Lb, calibratedCi95Ub)))
+                               sprintf("%0.2f (%0.2f - %0.2f)", calibratedRr, calibratedCi95Lb, calibratedCi95Ub)),
+               origin = 0)
+    vizData <- vizData |>
+        full_join(tibble(databaseId = databaseIds), by = join_by(databaseId))
     vizData$databaseId <- databaseIdToFactor(vizData$databaseId, databaseIds)
 
     breaks <- c(0.5, 1, 2)
-    plot4 <- ggplot(vizData, aes(x = log(calibratedRr))) +
-        geom_vline(xintercept = 0) +
+    breaksDb <- vizData |>
+        filter(!is.na(calibratedRr)) |>
+        select(databaseId) |>
+        cross_join(tibble(x = log(breaks)))
+    plotEstimate <- ggplot(vizData, aes(x = log(calibratedRr))) +
+        geom_segment(aes(x = x, y = 0, xend = x, yend = 2), data = breaksDb, color = "lightgray") +
+        geom_vline(aes(xintercept = origin)) +
         geom_point(aes(color = type), shape = 16, y = -0.5) +
         geom_errorbarh(aes(xmin = log(calibratedCi95Lb), xmax = log(calibratedCi95Ub), color = type, y = -0.5, height = 0.5)) +
-        geom_label(aes(label = label), x = 0, y = 1, vjust = 1, label.size = 0) +
+        geom_label(aes(label = label), x = 0, y = 1, vjust = 1, label.size = 0, size = 3) +
         coord_cartesian(xlim = log(c(0.25, 4)), ylim = c(-1, 1)) +
         scale_x_continuous("Hazard ratio", breaks = log(breaks), labels = breaks) +
         scale_color_manual(values = "black") +
@@ -306,9 +341,7 @@ createPerDbResultsTable <- function(target, comparator, outcome, connection) {
             axis.text.y = element_blank(),
             axis.title.y = element_blank(),
             axis.ticks.y = element_blank(),
-            panel.grid.major.y = element_blank(),
-            panel.grid.minor.y = element_blank(),
-            panel.grid.major.x = element_line(color = "lightgray"),
+            panel.grid = element_blank(),
             panel.background = element_blank(),
             strip.background = element_blank(),
             strip.text.y.left = element_blank(),
@@ -316,11 +349,11 @@ createPerDbResultsTable <- function(target, comparator, outcome, connection) {
             legend.title = element_blank(),
             legend.key.size = unit(0.25, "cm")
         )
-    plot4
+    plotEstimate
 
+    # Combine plots --------------------------------------------------------------------------------
+    plot <- grid.arrange(plotEquipoise, plotBalance, plotMdrr, plotEase, plotEstimate, ncol = 5, widths = c(1.0, 0.6, 0.33, 0.6, 0.6))
 
-    plot <- grid.arrange(plot1, plot2, plotMdrr, plot3, plot4, ncol = 5, widths = c(1, 0.5, 0.25, 0.5, 0.5))
-
-    ggsave("plot.png", plot = plot, width = 8, height = 5, dpi = 300)
+    ggsave("plot.png", plot = plot, width = 8.5, height = 5, dpi = 300)
 }
 
